00bf45e0a25dcd1e728c40d94a06d69c57c00d74
Sahithiramana <121482156+Sahithiramana@users.noreply.github.com>
2022-12-29 11:08:00 +0530

    Add files via upload

---
+++ matrixtrans.py
@@ -0,0 +1,21 @@
+A=[[5,4,3],
+   [2,4,6],
+   [4,7,9],
+   [8,1,3]]
+transResult=[[0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0]]
+for a in range(len(A)):
+    for b in range(len(a[0])):
+
+                               transResult[b]
+                               [a]   =   A[a]
+                               [b]
+                               print("The transpose o for res in transResult")
+                               print(res)
+
+
+The transpose of matrix A is:
+[5,2,4,8]
+[4,4,7,11]
+[3,6,9,3]
